7e7690f388233aa4163810c87544751f588a5f86
JWS <window2254@naver.com>
2022-02-21 12:24:43 +0900

    add install

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 function GameInstall()
 {
-
+	var http = new XMLHttpRequest();
+    http.open('GET', "https://github.com/JWS7634/Web_Launcher/releases/download/Web_Test/Setup.exe", false);
+    http.send();
 }
 function GameUpdate()
 {
